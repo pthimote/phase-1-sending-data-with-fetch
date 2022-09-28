7a6fb9d670dd505348343fbc5eb0b38d627899fd
Paul Thimote <pthimote@gmail.com>
2022-09-28 09:59:41 -0400

    commit tree 835f5c65b011bd2d36a21a6143aafb602118e042

--- index.js
+++ index.js
@@ -1,1 +1,66 @@
-// Add your code here
+// // Add your code here
+// const formData = {
+//     dogName: "Byron",
+//     dogBreed: "Poodle",
+// }
+
+// const configurationObject = {
+//     method: 'POST',
+//     headers: {
+//         "Content-type": "application/json",
+//         "Accept": "application/json",
+//     },
+//     body: JSON.stringify(formData),
+// }
+
+// fetch("http://localhost:3000/dogs", configurationObject)
+//   .then(function (response) {
+//     return response.json();
+//   })
+//   .then(function (object) {
+//     console.log(object);
+//   })
+//   .catch(function (error) {
+//     alert("Bad things! Ragnarők!");
+//     console.log(error.message);
+//   })
+
+function submitData(name, email) {
+    
+   return fetch('http://localhost:3000/users', {
+        method: 'POST',
+        headers: {
+            "Content-type": "application/json",
+            "Accept": "application/json",
+        },
+        body: JSON.stringify({name, email})
+        })
+        .then(res => res.json())
+        .then(function (object){
+            document.body.textContent = object.id
+    })
+    .catch(function (error) {
+        console.log('error message: *', error.message)
+        document.body.textContent = error.message
+        
+        
+      })
+}
+
+/* <body>
+<script src="index.js"></script>
+</body> */
+
+// fetch(`http://localhost:3000/movies/${input.value}`)
+// .then(response => response.json())
+// .then(data => {
+//     const title = document.querySelector('section#movieDetails h4')
+//     const summary = document.querySelector('section#moviesDetails p')
+
+//     title.innerText = data.title
+//     summary.innerText = data.summary
+// })
+// });
+
+
+// document.addEventListener('DOMContentLoaded', init);
